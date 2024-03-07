c790192d96f3dd53a7850bd2fe8852f051154fea
sathishSucess <sathishmec97@outlook.com>
2024-03-07 20:22:43 +0530

    2 commit

--- Java/src/test/java/org/javacode/SampleCodes.java
+++ Java/src/test/java/org/javacode/SampleCodes.java
@@ -1,25 +1,106 @@
 package org.javacode;
 
+import java.util.Scanner;
+
 public class SampleCodes {
 
 	
 public static void main(String[] args) {
 	
+	// Arm Strong number 
 	
-	int a[]= {10,20,30,80,60,70,40,50};
+//	Scanner sc=new Scanner(System.in);
+//  int arm=0;
+//  System.out.println("Enter Ur number ");
+//	
+//  int num=sc.nextInt();
+//  int temp=num;
+//	while(num>0) {
+//		int n=num%10;
+//		arm=arm+(n*n*n);
+//		num/=10;
+//	}
+//	
+//	if(arm==temp) {
+//		System.out.println("ArmStrong number ");
+//	}
+//	else {
+//		System.out.println("Not Armstrong number");
+//	}
 	
-	for(int i=0;i<a.length;i++) {
-		for(int j=i+1;j<a.length;j++) {
-			if(a[i]>a[j]) {
-				int temp=a[i];
-				a[i]=a[j];
-				a[j]=temp;
-			}
-		}
-	}
-	for(int i=0;i<a.length;i++) {
-		System.out.println(a[i]);
-	}
+	
+	//program for count and print armstrong number
+	
+	
+//	int count=0;
+//	for(int i=1;i<1000;i++) {
+//		int num=i;
+//		int arm=0;
+//		while(num>0) {
+//			int n=num%10;
+//			arm=arm+(n*n*n);
+//			num/=10;
+//		}
+//		if(arm==i) {
+//			System.out.println("ArmStrong Number: "+i);
+//			count++;
+//		}
+//	}
+//	System.out.println("Count of Armstrong Number: "+count);
+//	
+	
+	// Ascending order Number program
+	
+//	int a[]= {30,50,30,70,10,20,40,60};
+//	
+//	for(int i=0;i<a.length;i++) {
+//		for(int j=i+1;j<a.length;j++) {
+//			if(a[i]>a[j]) {
+//				int temp=a[i];
+//				a[i]=a[j];
+//				a[j]=temp;
+//			}
+//		}
+//	}
+//	
+//	for(int i=0;i<a.length;i++) {
+//		System.out.println(a[i]);
+//	}
+//	System.out.println("Maximum value in arry: "+a[a.length-1]);
+//	System.out.println("Maximum 2nd value in Array: "+a[a.length-2]);
+//	System.out.println("Miminum Value in array: "+a[0]);
+//	System.out.println("Minimum 2nd value In Array: "+a[1]);
+	
+	
+	// Desending order Array
+	
+//	int a[]= {10,40,50,30,20};
+//	
+//	for(int i=0;i<a.length;i++) {
+//		for(int j=i+1;j<a.length;j++) {
+//			if(a[i]<a[j]) {
+//				int temp=a[i];
+//				a[i]=a[j];
+//				a[j]=temp;
+//			}
+//		}
+//	}
+//	for(int i=0;i<a.length;i++) {
+//		System.out.println(a[i]);
+//	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
